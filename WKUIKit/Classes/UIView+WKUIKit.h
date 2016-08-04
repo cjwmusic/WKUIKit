9d11df80f71a2a971211aacef5157efb2892168d
wukong <caijunwei1990@163.com>
2016-08-04 16:27:14 +0800

    Add UIView Category.

---
+++ WKUIKit/Classes/UIView+WKUIKit.h
@@ -0,0 +1,20 @@
+//
+//  UIView+WKUIKit.h
+//  Pods
+//
+//  Created by Wu Kong on 8/4/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (WKUIKit)
+
+@property (nonatomic, assign) CGFloat left;
+@property (nonatomic, assign) CGFloat right;
+@property (nonatomic, assign) CGFloat top;
+@property (nonatomic, assign) CGFloat bottom;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+
+@end
