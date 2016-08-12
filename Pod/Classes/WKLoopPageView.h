f24578530449dc996be6028fb96fa33ca394ff11
Wu Kong <caijunwei1990@163.com>
2016-08-12 20:28:07 +0800

    添加图片滚动播放的视图.

---
+++ Pod/Classes/WKLoopPageView.h
@@ -0,0 +1,13 @@
+//
+//  WKLoopPageView.h
+//  Pods
+//
+//  Created by Wu Kong on 8/12/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WKLoopPageView : UIView
+
+@end
---
+++ Pod/Classes/WKLoopPageView.m
@@ -0,0 +1,13 @@
+//
+//  WKLoopPageView.m
+//  Pods
+//
+//  Created by Wu Kong on 8/12/16.
+//
+//
+
+#import "WKLoopPageView.h"
+
+@implementation WKLoopPageView
+
+@end
